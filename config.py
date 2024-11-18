943bc6ebf50e21cbbab4568cf6fb8ce452155262
Ruslan Mamakhaev <mamakhaev00@mail.ru>
2024-11-18 11:18:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,9 +6,7 @@ from datetime import datetime as dt
 # load_dotenv('/app/utils/config/.env')
 load_dotenv(find_dotenv(".env"))
 
-# Список ID администраторов
-admin_ids = [820372096]  # Замените на реальные ID администраторов
-# Подключение к БД
+admin_ids = [820372096]
 
 async def get_time() -> str:
     """Возвращает сегодняшнюю дату в строковом виде в формате  ДД.ММ.ГГГГ"""
